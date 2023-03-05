67d591335ecc160e60875a8d5b91f90be9e2007b
David <dfonsecar@esliceu.net>
2023-03-05 23:51:57 +0100

    commit_final

--- botiga_de_roba/home.php
+++ botiga_de_roba/home.php
@@ -8,13 +8,16 @@
 ?>
 <section class="jumbo1">
     <h3 class="tittle2">Visita les nostres seccions</h3>
+    <img src="/botiga_de_roba/imgs/jumbo-botiga.png">
 </section>
 <section class="jumbo-bottom flex">
     <section class="images flex">
         <section class="cuadre">
-            <div class="samarretes"></div>
+            <div class="samarretes">
+                <img src="/botiga_de_roba/imgs">
+            </div>
             <div class="blanc1 flex">
-                <h1 class="subtitol">samarretes a la moda Samarretes
+                <h1 class="subtitol">Samarretes a la moda Samarretes
                 </h1>
 </section>
 <section class="cuadre">
@@ -24,3 +27,6 @@
                 </h1>
 </div>
 </section>
+<?php
+    get_footer ();
+?>
