f17fbd44a9d371cab00c5f6477398cadbd32338a
David <dfonsecar@esliceu.net>
2023-03-05 19:58:45 +0100

    commit_4

--- botiga_de_roba/home.php
+++ botiga_de_roba/home.php
@@ -7,9 +7,20 @@
     get_header ();
 ?>
 <section class="jumbo1">
-    <h3 class="tittle2">VIsita les nostres seccions</h3>
+    <h3 class="tittle2">Visita les nostres seccions</h3>
 </section>
 <section class="jumbo-bottom flex">
     <section class="images flex">
         <section class="cuadre">
             <div class="samarretes"></div>
+            <div class="blanc1 flex">
+                <h1 class="subtitol">samarretes a la moda Samarretes
+                </h1>
+</section>
+<section class="cuadre">
+        <div class="texans"></div>
+            <div class="blanc2 flex">
+                <h1 class="subtitol">els nostres texans Texans
+                </h1>
+</div>
+</section>
